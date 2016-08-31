8aeaf4a718e8b53192d7afc53130bbcfb42ba343
Joe Parente <jparente@deltatg.com>
2016-08-31 15:37:46 -0400

    tui

--- src/Transunion/Transunion.php
+++ src/Transunion/Transunion.php
@@ -189,6 +189,7 @@ class Transunion {
             curl_setopt($ch, CURLOPT_POSTFIELDS, $xml);
 
             curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+            curl_setopt($ch, CURLCA_BUNDLE, '/etc/ssl/certs/');
             curl_setopt($ch, CURLOPT_SSLCERT, $this->certificate['crt']);
             curl_setopt($ch, CURLOPT_SSLKEY, $this->certificate['key']);
             curl_setopt($ch, CURLOPT_SSLCERTPASSWD, $this->certificate['password']);
